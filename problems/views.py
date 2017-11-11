8240fa47b43142a830fe2e9bd08a750a899715ed
Egaliciar <tnt_code@hotmail.com>
2017-11-10 21:55:01 -0600

    lista de preguntas aleatorias

--- problems/views.py
+++ problems/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .functions import LIST_PROBLEMS
+from random import randint
 # Create your views here.
 
 
 def problems(request):
-    return render(request, 'index.html')
+    random_numbre = randint(0,len(LIST_PROBLEMS)-1)
+    problema = LIST_PROBLEMS[random_numbre]
+    return render(request, 'index.html', problema)
